8f0f6bbf25ad026f6c73b1136cecc7b404057a56
inommm <ryo.inom@gmail.com>
2020-04-20 00:07:02 +0900

    update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -9,10 +9,10 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 17,
+    fontSize: 16,
 
     // font family with optional fallbacks
-    fontFamily: 'Ricty',
+    fontFamily: '"SF Mono Square"',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
